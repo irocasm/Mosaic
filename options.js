5326723af88f86d3dfdb7d3a4660e144f88484c2
irocasm <muell-post@web.de>
2014-01-04 23:36:44 +0100

    outsource options

---
+++ options.js
@@ -0,0 +1,9 @@
+var options = {
+	cellSize: 30,
+	fontSize: 22,
+	stateColors: ["transparent", "#000", "#fff"],
+	fontColors:  ["#000",        "#fff", "#000"],
+	gridColor: "#777",
+	correctNrColor: "#777",
+	thumbnailCellSize: 2
+};
